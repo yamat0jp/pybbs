c3db4baec266277e9deea252bc5cde58de544d45
yamat0jp <yamat0jp@yahoo.co.jp>
2016-11-19 23:54:34 +0900

    db.close()

--- index.py
+++ index.py
@@ -277,10 +277,14 @@ class Application(tornado.web.Application):
                         'ui_modules':{'Footer':FooterModule},
                         'cookie_secret':'bZJc2sWbQLKos6GkHn/VB9oXwQt8SOROkRvJ5/xJ89E=',
                         'xsrf_cookies':True,
-                        'debug':True,
+                        #'debug':True,
                         'login_url':'/login'
                         }
         tornado.web.Application.__init__(self,handlers,**settings)
+    
+    def __delattr__(self, *args, **kwargs):
+        self.db.close()
+        return tornado.web.Application.__delattr__(self, *args, **kwargs)
  
     def gpos(self,dbname,page):
         params = self.db.get(where('kinds') == 'conf')
