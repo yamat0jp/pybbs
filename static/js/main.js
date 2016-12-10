d2d13d25cf0885dae4f41a3ae2cb9c58e7945401
yamat0jp <yamat0jp@yahoo.co.jp>
2016-12-10 21:10:50 +0900

    rotate used I had mani bad commit sorry

---
+++ static/js/main.js
@@ -0,0 +1,18 @@
+var
+arg = {
+	'font-size':'2em',
+	'transform':'rotate(5deg)'
+};
+narg = {
+	'font-size':'initial',
+	'transform':'initial'
+};
+
+$(function(){
+	$('div').on('click',function(){
+		$(this).css(arg);
+	})
+	.on('mouseout',function(){
+		$(this).css(narg);
+	});
+});
