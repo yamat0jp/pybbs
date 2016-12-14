07d2bc2c81e31686b90904249479534446702ad4
yamat0jp <yamat0jp@yahoo.co.jp>
2016-12-14 19:42:51 +0900

    livePreview ok

--- static/js/main.js
+++ static/js/main.js
@@ -19,15 +19,3 @@ $(function(){
 		$(this).css(narg);
 	});
 });
-$(document).ready(function(){
-	$('.livepreview').livePreview({
-	    trigger: 'hover',
-	    viewWidth: 300,  
-	    viewHeight: 200,  
-	    targetWidth: 1000,  
-	    targetHeight: 800,  
-	    scale: '0.5', 
-	    offset: 50,
-	    position: 'left'
-	});
-});
