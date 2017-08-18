750d2f6de8dd448e9165fbbdebbb4dff789dff01
yamat0jp <yamat0jp@yahoo.co.jp>
2017-08-18 16:09:51 +0900

    スパム対策

--- index.py
+++ index.py
@@ -48,6 +48,7 @@ class IndexHandler(BaseHandler):
                 raise tornado.web.HTTPError(404)
                 return
         i = params['count']      
+        rule = tornado.escape.url_unescape(self.get_cookie('aikotoba',''))
         na = tornado.escape.url_unescape(self.get_cookie("username",u"誰かさん"))
         pos = self.application.gpos(dbname,page)
         table = self.application.db.table(dbname)
