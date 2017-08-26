16d88d2b62ff7dd5510ae94b227928dd4049da05
yamat0jp <yamat0jp@yahoo.co.jp>
2017-08-26 19:28:53 +0900

    unuse cookie in search and delete unescape function

--- index.py
+++ index.py
@@ -315,19 +315,17 @@ class UserHandler(tornado.web.RequestHandler):
       
 class SearchHandler(tornado.web.RequestHandler):       
     def post(self,dbname):
-        self.word = tornado.escape.url_unescape(self.get_argument('word1'))
-        self.radiobox = self.get_argument('filter')
-        self.set_cookie('search',tornado.escape.url_escape(self.word,False))         
+        arg = self.get_argument('word1')
+        self.word = arg 
+        self.radiobox = self.get_argument('filter')      
         rec = sorted(self.search(dbname),key=lambda x: x['number'])
-        self.render('modules/search.htm',records=rec,word1=self.word,db=dbname)
+        self.render('modules/search.htm',records=rec,word1=arg,db=dbname)
     
     def get(self,dbname):
         if self.application.collection(dbname) == False:
             raise tornado.web.HTTPError(404)
             return
-        word = self.get_cookie('search','')
-        word = tornado.escape.url_unescape(word)
-        self.render('modules/search.htm',records=[],word1=word,db=dbname)
+        self.render('modules/search.htm',records=[],word1='',db=dbname)
         
     def search(self,dbname):
         table = self.application.db.table(dbname)    
