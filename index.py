4477aee65230543814e4ddbdc95640b40410a37a
yamat0jp <yamat0jp@yahoo.co.jp>
2017-08-27 15:45:23 +0900

    replace

--- index.py
+++ index.py
@@ -317,15 +317,22 @@ class SearchHandler(tornado.web.RequestHandler):
             element = ['']
         while len(element) < 3:
             element.append(element[0])
+        elm = []
+        for i in range(3):
+            s = element[i]
+            for x in s:
+                if x in '?()[]+*':
+                    s.replace(x,'\\'+x)
+            elm.append(s)
         if self.radiobox == 'comment':    
-            for x in table.find({'$or':[{'raw':re.compile(element[0],re.IGNORECASE)},
-                                        {'raw':re.compile(element[1],re.IGNORECASE)},
-                                        {'raw':re.compile(element[2],re.IGNORECASE)}
+            for x in table.find({'$or':[{'raw':re.compile(elm[0],re.IGNORECASE)},
+                                        {'raw':re.compile(elm[1],re.IGNORECASE)},
+                                        {'raw':re.compile(elm[2],re.IGNORECASE)}
                                         ]}):
                 com = ''
                 for text in x['raw'].splitlines(True):                  
-                    for y in range(3):                        
-                        if (len(element[y]) > 0)and(text.lower().find(element[y].lower()) > -1):
+                    for y in elm:                        
+                        if text.lower().find(y.lower()) > -1:
                             com = com +'<p style=background-color:yellow>'+text+'<br></p>'  
                             break                          
                     else:
