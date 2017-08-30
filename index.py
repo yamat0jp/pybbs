84a624a46d9d75fdc089ec70007a0675ff57a2b8
yamat0jp <yamat0jp@yahoo.co.jp>
2017-08-31 06:50:30 +0900

    login query better

--- index.py
+++ index.py
@@ -68,7 +68,7 @@ class IndexHandler(BaseHandler):
         
 class LoginHandler(BaseHandler):
     def get(self):
-        query = self.get_query_argument('next')
+        query = self.get_query_argument('next','')
         i = query[1:].find('/')+1
         qs = query[1:i]
         self.render('login.htm',db=qs)
