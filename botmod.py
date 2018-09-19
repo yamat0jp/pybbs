e7743e3822b71e01948cfb7854e4030439f46533
福家 正士 <yamat0jp@yahoo.co.jp>
2018-09-19 18:21:18 +0900

    join の存在を忘れる

--- botmod.py
+++ botmod.py
@@ -67,10 +67,7 @@ class WebHookHandler(tornado.web.RequestHandler):
         if dbname[-4:] == '_bot' and dbname in ca:
             db = self.database['users_bot']
             item = db.find_one({'name':self.uid})
-            if not item:
-                db.insert({'name':self.uid, 'dbname':dbname})
-                return True
-            elif item['dbname'] == dbname:
+            if item['dbname'] == dbname:
                 return False
             else:
                 db.update({'name':self.uid}, {'name':self.uid, 'dbname':dbname})
