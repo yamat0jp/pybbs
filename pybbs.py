5b865c32c447487231a5226bc2014108db3d0a21
yamat0jp <terukohietori@gmail.com>
2019-03-08 19:24:59 +0900

    特に問題なし

--- pybbs.py
+++ pybbs.py
@@ -105,7 +105,7 @@ class NaviHandler(web.RequestHandler):
             item = {"mentenance":False,"out_words":[u"阿保",u"馬鹿",u"死ね"],"password":"admin",
                     "title2":"<h1 style=color:gray;text-align:center>pybbs</h1>",
                     "bad_words":["<style","<link","<script","<img"],"count":30,
-                    "title":"pybbs","info name":"info","kinds":"conf"}
+                    "title":"pybbs","info name":"info","kinds":"conf","app":"bbs"}
             self.application.db.insert(item)
             self.application.db.table('info')
         elif data['mentenance'] is True:
